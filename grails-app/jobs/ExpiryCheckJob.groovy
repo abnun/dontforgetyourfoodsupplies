81fa6590e74ca80804fbd2e2ff53716eec7fcc49
Markus <abnun@gmx.de>
2009-08-12 22:26:13 +0200

    fixed error in job

--- grails-app/jobs/ExpiryCheckJob.groovy
+++ grails-app/jobs/ExpiryCheckJob.groovy
@@ -7,7 +7,7 @@ class ExpiryCheckJob
 {
 	static triggers =
 	{
-		simple(name: 'expiryCheck', cronExpression: "0 0 0 * * ?")
+		cron(name: 'expiryCheck', cronExpression: '0 0 0 * * ?')
 		//simple(name:'expiryCheck', startDelay: 10000, repeatInterval: 5000l)
 	}
 	// execute job once in 5 seconds
